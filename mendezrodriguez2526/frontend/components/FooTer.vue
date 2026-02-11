54e4a4062c2c8fe3610ef7da8913df043eb2fe3b
Andre <andremrrv@gmail.com>
2026-02-11 11:26:33 +0100

    Post-revision

--- mendezrodriguez2526/frontend/components/FooTer.vue
+++ mendezrodriguez2526/frontend/components/FooTer.vue
@@ -1,11 +1,26 @@
 <template>
   <footer class="navbar navbar-dark bg-danger position-sticky top-0">
     <div class="container">
-      <p class="mb-1 text-white">&copy; {{ new Date().getFullYear() }} MiSitio. Todos los derechos reservados.</p>
+      <p class="mb-1 text-white">
+        &copy; {{ new Date().getFullYear() }} MiSitio. Todos los derechos
+        reservados.
+      </p>
       <ul class="list-inline mb-0">
-        <li class="list-inline-item"><router-link class="nav-link" to="/AvisoLegal">Aviso Legal</router-link></li>
-        <li class="list-inline-item"><router-link class="text-light text-decoration-none" to="/PoliticaPrivacidad">Política de privacidad</router-link></li>
-        <li class="list-inline-item"><a href="#" class="text-light text-decoration-none">Contacto</a></li>
+        <li class="list-inline-item">
+          <router-link class="nav-link" to="/AvisoLegal"
+            >Aviso Legal</router-link
+          >
+        </li>
+        <li class="list-inline-item">
+          <router-link
+            class="text-light text-decoration-none"
+            to="/PoliticaPrivacidad"
+            >Política de privacidad</router-link
+          >
+        </li>
+        <li class="list-inline-item">
+          <a href="#" class="text-light text-decoration-none">Contacto</a>
+        </li>
       </ul>
     </div>
   </footer>
